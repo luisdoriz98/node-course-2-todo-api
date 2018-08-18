487c6063abafe8e76bec43751f6817b5b95ba309
Luis <luis.doriz@udem.edu>
2018-08-18 18:02:40 -0500

    Setup app to heroku1

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+// mongoose.connect('mongodb://localhost:27017/TodoApp');
+ mongoose.connect(process.env.'mongodb://luisd:roble219@ds225902.mlab.com:25902/elavoro1' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
