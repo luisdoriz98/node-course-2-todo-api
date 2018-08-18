91ebbeb9c27082f8580e73cc806977fd0674574e
Luis <luis.doriz@udem.edu>
2018-08-18 18:12:40 -0500

    das

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 // mongoose.connect('mongodb://localhost:27017/TodoApp');
- mongoose.connect(process.env.'mongodb://luisd:roble219@ds225902.mlab.com:25902/elavoro1' || 'mongodb://localhost:27017/TodoApp');
+ mongoose.connect('mongodb://[luisd:roble219@]ds225902.mlab.com:25902/elavoro1');
 
 module.exports = {mongoose};
