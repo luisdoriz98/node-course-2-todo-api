7aa6d2682a027eb5b8be9632a574e56bf36e8158
Luis <luis.doriz@udem.edu>
2018-08-18 18:33:22 -0500

    heroku finally

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-// mongoose.connect('mongodb://localhost:27017/TodoApp');
- mongoose.connect('mongodb://[luisd:roble219@]ds225902.mlab.com:25902/elavoro1');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
