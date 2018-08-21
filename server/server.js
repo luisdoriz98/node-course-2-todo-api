31ca53e432c2375b8940a4cd80170de6684790bd
Luis <luis.doriz@udem.edu>
2018-08-21 00:55:07 -0500

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-const {ObjectID} = require('mongodb');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
-
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
@@ -40,6 +39,21 @@ app.get('/todos/:id', (req, res) => {
     };
 
     Todo.findById(id).then((todo) => {
+      if (!todo) {
+        return res.status(404).send();
+      };
+      res.send({todo});
+    }).catch((e) => res.status(400).send());
+});
+
+
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+    if (!ObjectID.isValid(id)){
+      return res.status(404).send();
+    };
+
+    Todo.findByIdAndRemove(id).then((todo) => {
       if (!todo) {
         return res.status(404).send();
       };
